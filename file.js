75a223a42946a20a29b5e19288c89a0cd95691ff
Aleah Manzi <aleah.manzi@gmail.com>
2016-10-12 23:19:49 -0400

    displaying number of questions correct.

--- file.js
+++ file.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 var whichAsk = 0; 
 var askAnswer = "";
-var number = "";
+var number = 0;
 
 $(".start_text").click(function(){
     $(".start_wrap").hide();
@@ -49,27 +49,32 @@ $(".submit_text").click(function(){
   function checkAnswer(answer){
     if (whichAsk == 1 && answer == "answer_b"){
       console.log("answer one is right");
+      number = number+1
       q1Hide();
       q2Show();
     }
     else if (whichAsk == 1 && answer !== "answer_b"){
       console.log("answer one is wrong");
+      number = 0;
       q1Hide();
       q2Show();
     }
     else if (whichAsk == 2 && answer == "answer_a") {
       console.log("answer two is right");
-
+      number = number+1
       q2Hide();
       q3Show();
     }
     else if (whichAsk == 2 && answer !== "answer_a") {
       console.log("answer two is wrong");
+      number = number;
       q2Hide();
       q3Show();
     }
     else if (whichAsk == 3 && answer == "answer_c") {
       console.log("answer three is right");
+      number = number+1
+      $(".number").append(number);
       $(".results").show();
       $(".submit_wrap").hide();
       $(".ask_three").hide();  
@@ -77,6 +82,7 @@ $(".submit_text").click(function(){
     }
     else if (whichAsk == 3 && answer !== "answer_c") {
       console.log("answer three is wrong");
+      $(".number").append(number);
       $(".results").show();
       $(".submit_wrap").hide();
       $(".ask_three").hide();  
@@ -92,6 +98,6 @@ $(".submit_text").click(function(){
     whichAsk = 0; 
     number = 0;
     askAnswer = 0;
-});/// - start game, show first question
+});/// - restart game
 
 });
